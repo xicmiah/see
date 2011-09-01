9c5ffb25cd8c9a9c2852a4e5b778ca119a1055ef
xicmiah <xicmiah@gmail.com>
2011-09-01 15:03:18 +0400

    Added facade

---
+++ src/main/java/see/See.java
@@ -0,0 +1,68 @@
+package see;
+
+import org.parboiled.Parboiled;
+import org.parboiled.Rule;
+import see.evaluator.SimpleEvaluator;
+import see.parser.BasicParser;
+import see.parser.Parser;
+import see.parser.grammar.Expressions;
+import see.tree.Node;
+
+import java.util.Map;
+
+/**
+ * Facade to parse/evaluate operations.
+ * All operations are thread-safe. Parse results are immutable and can be reused/cached between different instances.
+ */
+public class See {
+    /**
+     * Parse a single expression
+     *
+     * @param expression text to parse
+     * @return parsed tree
+     */
+    public Node<Object> parseExpression(String expression) {
+        return parse(expression, getGrammar().Condition());
+    }
+
+    /**
+     * Parse semicolon-separated list of expressions
+     *
+     * @param expression text to parse
+     * @return parsed tree
+     */
+    public Node<Object> parseExpressionList(String expression) {
+        return parse(expression, getGrammar().Statements());
+    }
+
+    /**
+     * Parse a expression with 'return' keyword
+     *
+     * @param expression text to parse
+     * @return parsed tree
+     */
+    public Node<Object> parseReturnExpression(String expression) {
+        return parse(expression, getGrammar().CalcExpression());
+    }
+
+    /**
+     * Evaluate tree with supplied variables
+     *
+     * @param tree tree to evaluate
+     * @param context variable->value mapping
+     * @param <T> return type
+     * @return evaluated value
+     */
+    public <T> T evaluate(Node<T> tree, Map<String, Object> context) {
+        return new SimpleEvaluator().evaluate(tree, context);
+    }
+
+    private Node<Object> parse(String expression, Rule rule) {
+        Parser<Object> parser = new BasicParser<Object>(rule);
+        return parser.parse(expression);
+    }
+
+    private Expressions getGrammar() {
+        return Parboiled.createParser(Expressions.class);
+    }
+}
---
+++ src/main/java/see/parser/BasicParser.java
@@ -0,0 +1,29 @@
+package see.parser;
+
+import org.parboiled.Rule;
+import org.parboiled.parserunners.ParseRunner;
+import org.parboiled.parserunners.ReportingParseRunner;
+import org.parboiled.support.ParsingResult;
+import see.tree.Node;
+
+public class BasicParser<T> implements Parser<T> {
+
+    private final Rule rule;
+
+    public BasicParser(Rule rule) {
+        this.rule = rule;
+    }
+
+    @Override
+    public Node<T> parse(String input) {
+        ParseRunner<Node<T>> runner = new ReportingParseRunner<Node<T>>(rule);
+        ParsingResult<Node<T>> result = runner.run(input);
+        
+        if (!result.matched) {
+            // TODO: add proper exception hierarchy
+            throw new RuntimeException("Syntax error");
+        }
+
+        return result.resultValue;
+    }
+}
