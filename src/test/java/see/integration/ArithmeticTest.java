5778ad331d0e880d32ee9dffd38e0358dd89338f
xicmiah <xicmiah@gmail.com>
2011-09-06 12:36:30 +0400

    Extended integration tests

--- src/test/java/see/integration/ArithmeticTest.java
+++ src/test/java/see/integration/ArithmeticTest.java
@@ -1,58 +1,82 @@
 package see.integration;
 
 import com.google.common.collect.ImmutableMap;
+import org.junit.Before;
 import org.junit.Test;
 import see.See;
-import see.tree.Node;
+import see.functions.Function;
+import see.parser.config.ConfigBuilder;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 import static java.math.BigDecimal.valueOf;
 import static org.junit.Assert.assertEquals;
 
 public class ArithmeticTest {
-    private final BigDecimal nine = valueOf(9);
+    final BigDecimal nine = valueOf(9);
+    
+    See see;
 
-    protected Object eval(String expression, Map<String, Object> context) {
-        See see = new See();
-        Node<Object> tree = see.parseExpression(expression);
-        return see.evaluate(tree, context);
-    }
-
-    protected Object eval(String expression) {
-        return eval(expression, new HashMap<String, Object>());
+    @Before
+    public void setUp() throws Exception {
+        see = new See(ConfigBuilder.defaultConfig().addPureFunction("fail", new Function<List<Object>, Object>() {
+            @Override
+            public Object apply(List<Object> input) {
+                throw new IllegalStateException("Fail evaluated");
+            }
+        }).build());
     }
 
     @Test
     public void testArithmetic() throws Exception {
-        assertEquals(nine, eval("4+5"));
-        assertEquals(nine, eval("42-33"));
-        assertEquals(nine, eval("3*3"));
-        assertEquals(nine, eval("54/6"));
+        assertEquals(nine, see.eval("4+5"));
+        assertEquals(nine, see.eval("42-33"));
+        assertEquals(nine, see.eval("3*3"));
+        assertEquals(nine, see.eval("54/6"));
         
-        assertEquals(nine, eval("4 + 2 * 3 - 5 / 5"));
-        assertEquals(nine, eval("((4 + (2*3)) - (5/5))"));
+        assertEquals(nine, see.eval("4 + 2 * 3 - 5 / 5"));
+        assertEquals(nine, see.eval("((4 + (2*3)) - (5/5))"));
     }
 
     @Test
     public void testUnaryPlusMinus() throws Exception {
-        assertEquals(valueOf(-9), eval("-9"));
-        assertEquals(valueOf(9), eval("+9"));
-        assertEquals(valueOf(9), eval("-(-9)"));
+        assertEquals(valueOf(-9), see.eval("-9"));
+        assertEquals(valueOf(9), see.eval("+9"));
+        assertEquals(valueOf(9), see.eval("-(-9)"));
     }
 
     @Test
     public void testLogicalOps() throws Exception {
-        assertEquals(valueOf(0), eval("!9"));
-        assertEquals(valueOf(0), eval("!1"));
-        assertEquals(valueOf(1), eval("!0"));
+        assertEquals(valueOf(0), see.eval("!9"));
+        assertEquals(valueOf(0), see.eval("!1"));
+        assertEquals(valueOf(1), see.eval("!0"));
+
+        assertEquals(valueOf(0), see.eval("0 || 0"));
+        assertEquals(valueOf(1), see.eval("1 || 0"));
+        assertEquals(valueOf(1), see.eval("0 || 1"));
+        assertEquals(valueOf(1), see.eval("1 || 1"));
+ 
+        assertEquals(valueOf(0), see.eval("0 && 0"));
+        assertEquals(valueOf(0), see.eval("1 && 0"));
+        assertEquals(valueOf(0), see.eval("0 && 1"));
+        assertEquals(valueOf(1), see.eval("1 && 1"));
+    }
+
+    @Test
+    public void testLogicalShortCircuit() throws Exception {
+        assertEquals(valueOf(0), see.eval("0 && fail()"));
+        assertEquals(valueOf(1), see.eval("1 || fail()"));
     }
 
     @Test
     public void testIdentifiers() throws Exception {
-        assertEquals(nine, eval("a-b", ImmutableMap.<String, Object>of("a", valueOf(10), "b", valueOf(1))));
-        assertEquals(nine, eval("a_b", ImmutableMap.<String, Object>of("a_b", valueOf(9))));
+        ImmutableMap<String, Object> context = ImmutableMap.<String, Object>builder()
+                .put("a", valueOf(10))
+                .put("b", valueOf(1))
+                .put("a_b", valueOf(9))
+                .build();
+        assertEquals(nine, see.eval("a-b", context));
+        assertEquals(nine, see.eval("a_b", context));
     }
 }
