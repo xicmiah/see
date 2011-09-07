5f73d33963d662699d3c18746e4cefa53646955d
xicmiah <xicmiah@gmail.com>
2011-09-07 10:55:42 +0400

    SimpleEvaluator now doesn't wrap EvaluationException subclasses

--- src/main/java/see/evaluator/SimpleEvaluator.java
+++ src/main/java/see/evaluator/SimpleEvaluator.java
@@ -13,11 +13,23 @@ public class SimpleEvaluator implements Evaluator {
     public SimpleEvaluator(NumberFactory numberFactory) {
         this.numberFactory = numberFactory;
     }
-    
+
+    /**
+     * Evaluate tree with exception translation.
+     * Subclasses of [@link EvaluationException] are passed as-is, others are wrapped in [@link EvaluationException].
+     * 
+     * @param tree tree to evaluate
+     * @param context evaluation context
+     * @param <T> return type
+     * @return evaluation result
+     * @throws EvaluationException on error during evaluation
+     */
     @Override
     public <T> T evaluate(Node<T> tree, Map<String, ?> context) throws EvaluationException {
         try {
             return tree.accept(new ContextualVisitor(numberFactory, context));
+        } catch (EvaluationException e) {
+            throw e;
         } catch (Exception e) {
             throw new EvaluationException(e);
         }
