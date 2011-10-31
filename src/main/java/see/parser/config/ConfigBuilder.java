c934e139c3e13d6db1253f6deaee9ab8932f2347
xicmiah <xicmiah@gmail.com>
2011-10-31 17:21:37 +0400

    Split long list of declarations in ConfigBuilder

--- src/main/java/see/parser/config/ConfigBuilder.java
+++ src/main/java/see/parser/config/ConfigBuilder.java
@@ -9,10 +9,7 @@ import see.functions.bool.And;
 import see.functions.bool.Not;
 import see.functions.bool.Or;
 import see.functions.compare.*;
-import see.functions.properties.MakeIndexed;
-import see.functions.properties.MakeTarget;
-import see.functions.properties.PropertyFunctions;
-import see.functions.properties.PropertyUtilsResolver;
+import see.functions.properties.*;
 import see.functions.service.Assign;
 import see.functions.service.If;
 import see.functions.service.IsDefined;
@@ -43,58 +40,76 @@ public class ConfigBuilder {
         final ConfigBuilder builder = ConfigBuilder.emptyConfig();
         builder.setNumberFactory(new BigDecimalFactory());
 
-        builder.addAlias(";", "seq");
-        builder.addAlias("=", "assign");
+        addServiceFunctions(builder);
+        addLogic(builder);
+        addArithmetic(builder);
+        addCompare(builder);
+        addProperty(builder, new PropertyUtilsResolver());
 
-        builder.addAlias("!", "not");
-        builder.addAlias("&&", "and");
-        builder.addAlias("||", "or");
-        
+        return builder;
+    }
+
+    private static void addProperty(ConfigBuilder builder, PropertyResolver resolver) {
+        builder.addAlias(".", "get");
+        builder.addAlias("[]", "props.indexed");
+        builder.addAlias(".=", "set");
+
+        PropertyFunctions propertyFunctions = new PropertyFunctions(resolver);
+        builder.addPureFunction("get", propertyFunctions.getGetFunction());
+        builder.addPureFunction("set", propertyFunctions.getSetFunction());
+
+        builder.addPureFunction("props.target", new MakeTarget());
+        builder.addPureFunction("props.indexed", new MakeIndexed());
+    }
+
+    private static void addCompare(ConfigBuilder builder) {
+        builder.addPureFunction("==", new Eq());
+        builder.addPureFunction("!=", new Neq());
+        builder.addPureFunction(">", new Gt());
+        builder.addPureFunction(">=", new Geq());
+        builder.addPureFunction("<", new Lt());
+        builder.addPureFunction("<=", new Leq());
+    }
+
+    private static void addArithmetic(final ConfigBuilder builder) {
         builder.addAlias("+", "sum");
         builder.addAlias("-", "minus");
         builder.addAlias("*", "product");
         builder.addAlias("/", "divide");
         builder.addAlias("^", "pow");
 
-        builder.addAlias(".", "get");
-        builder.addAlias("[]", "props.indexed");
-        builder.addAlias(".=", "set");
-
-        builder.addFunction("seq", wrap(new Sequence<Object>()));
-        builder.addFunction("assign", wrap(new Assign<Object>()));
-        builder.addFunction("isDefined", (new IsDefined()));
-        builder.addPureFunction("if", (new If<Object>()));
-
-        PropertyFunctions propertyFunctions = new PropertyFunctions(new PropertyUtilsResolver());
-        builder.addPureFunction("get", propertyFunctions.getGetFunction());
-        builder.addPureFunction("set", propertyFunctions.getSetFunction());
-
-        builder.addPureFunction("props.target", new MakeTarget());
-        builder.addPureFunction("props.indexed", new MakeIndexed());
-
-        builder.addPureFunction("not", (new Not()));
-        builder.addPureFunction("and", (new And()));
-        builder.addPureFunction("or", (new Or()));
-        builder.addPureFunction("==", (new Eq()));
-        builder.addPureFunction("!=", (new Neq()));
-        builder.addPureFunction(">", (new Gt()));
-        builder.addPureFunction(">=", (new Geq()));
-        builder.addPureFunction("<", (new Lt()));
-        builder.addPureFunction("<=", (new Leq()));
-        builder.addPureFunction("min", (new Min<BigDecimal>()));
-        builder.addPureFunction("max", (new Max<BigDecimal>()));
-        builder.addPureFunction("sum", (new Sum()));
-        builder.addPureFunction("minus", (new Minus()));
-        builder.addPureFunction("product", (new Product()));
-        builder.addPureFunction("divide", (new Divide(new Supplier<MathContext>() {
+        builder.addPureFunction("min", new Min<BigDecimal>());
+        builder.addPureFunction("max", new Max<BigDecimal>());
+        builder.addPureFunction("sum", new Sum());
+        builder.addPureFunction("minus", new Minus());
+        builder.addPureFunction("product", new Product());
+        builder.addPureFunction("divide", new Divide(new Supplier<MathContext>() {
             @Override
             public MathContext get() {
                 return ((BigDecimalFactory) builder.numberFactory).getMathContext();
             }
-        }))); // Math context is passed by-name, will adapt to setNumberFactory() calls on builder
-        builder.addPureFunction("pow", (new Power()));
+        })); // Math context is passed by-name, will adapt to setNumberFactory() calls on builder
+        builder.addPureFunction("pow", new Power());
+    }
 
-        return builder;
+    private static void addLogic(ConfigBuilder builder) {
+        builder.addAlias("!", "not");
+        builder.addAlias("&&", "and");
+        builder.addAlias("||", "or");
+
+        builder.addPureFunction("not", new Not());
+        builder.addPureFunction("and", new And());
+        builder.addPureFunction("or", new Or());
+    }
+
+    private static void addServiceFunctions(ConfigBuilder builder) {
+        builder.addAlias(";", "seq");
+        builder.addAlias("=", "assign");
+
+        builder.addFunction("seq", wrap(new Sequence<Object>()));
+        builder.addFunction("assign", wrap(new Assign<Object>()));
+        builder.addFunction("isDefined", new IsDefined());
+        builder.addPureFunction("if", new If<Object>());
     }
 
     public static ConfigBuilder emptyConfig() {
