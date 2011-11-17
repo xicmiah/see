4a0f715441715e2ee459fd0c03d760d5ba9d7461
xicmiah <xicmiah@gmail.com>
2011-11-17 10:59:07 +0400

    Added Property interface

---
+++ src/main/java/see/functions/properties/Property.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2011 Vasily Shiyan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package see.functions.properties;
+
+import com.google.common.base.Supplier;
+import see.functions.Settable;
+
+/**
+ * Container holding one value of specified type.
+ * @param <T> value type
+ */
+public interface Property<T> extends Settable<T>, Supplier<T> {
+}
