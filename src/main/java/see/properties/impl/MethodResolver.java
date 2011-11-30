121ab15c35f0b1b82957e017f5d7a49877ce7cb9
xicmiah <xicmiah@gmail.com>
2011-11-30 15:22:25 +0400

    Added resolver method->closure

---
+++ src/main/java/see/properties/impl/MethodResolver.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2011 Vasily Shiyan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package see.properties.impl;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
+import org.apache.commons.beanutils.MethodUtils;
+import see.exceptions.EvaluationException;
+import see.functions.VarArgFunction;
+import see.parser.grammar.PropertyAccess;
+
+import javax.annotation.Nonnull;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public class MethodResolver extends ReadOnlyResolver {
+    @Override
+    public boolean canGet(Object target, PropertyAccess propertyAccess) {
+        final String methodName = Iterables.getOnlyElement(propertyAccess.value().left(), "");
+
+        return Iterables.any(asList(target.getClass().getMethods()), new Predicate<Method>() {
+            @Override
+            public boolean apply(Method input) {
+                return methodName.equals(input.getName());
+            }
+        });
+    }
+
+    @Override
+    public VarArgFunction<Object, ?> get(final Object bean, PropertyAccess property) {
+        final String methodName = Iterables.getOnlyElement(property.value().left(), "");
+
+        return new VarArgFunction<Object, Object>() {
+            @Override
+            public Object apply(@Nonnull List<Object> input) {
+                try {
+                    return MethodUtils.invokeMethod(bean, methodName, Iterables.toArray(input, Object.class));
+                } catch (NoSuchMethodException e) {
+                    throw new ResolutionException(e);
+                } catch (IllegalAccessException e) {
+                    throw new ResolutionException(e);
+                } catch (InvocationTargetException e) {
+                    throw new ResolutionException(e);
+                }
+            }
+        };
+    }
+
+    public static class ResolutionException extends EvaluationException {
+        public ResolutionException(String message) {
+            super(message);
+        }
+
+        public ResolutionException(String message, Throwable cause) {
+            super(message, cause);
+        }
+
+        public ResolutionException(Throwable cause) {
+            super(cause);
+        }
+    }
+}
---
+++ src/main/java/see/properties/impl/ReadOnlyResolver.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2011 Vasily Shiyan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package see.properties.impl;
+
+import see.parser.grammar.PropertyAccess;
+import see.properties.PartialResolver;
+
+public abstract class ReadOnlyResolver implements PartialResolver {
+    @Override
+    public boolean canSet(Object target, PropertyAccess propertyAccess, Object value) {
+        return false;
+    }
+
+    @Override
+    public void set(Object bean, PropertyAccess property, Object value) {
+        throw new UnsupportedOperationException("Set not supported");
+    }
+}
