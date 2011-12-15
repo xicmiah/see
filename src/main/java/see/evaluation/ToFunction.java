38c920eab00af2778e2c6ae3919b6c7ffbd7b69a
xicmiah <xicmiah@gmail.com>
2011-12-15 16:13:12 +0400

    Added partial function interface and convertion to function

---
+++ src/main/java/see/evaluation/ToFunction.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2011 Vasily Shiyan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package see.evaluation;
+
+import see.functions.Function;
+import see.functions.PartialFunction;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+public interface ToFunction extends PartialFunction<Object, Function<List<Object>, ?>> {
+
+    /**
+     * Try to convert given object to function.
+     * @param input object to convert
+     * @return function
+     */
+    @Override
+    @Nonnull
+    Function<List<Object>, ?> apply(@Nonnull Object input);
+
+    /**
+     * Return, if this object can be converted to function.
+     * @param input object to test
+     * @return true, if {@link #apply(Object)} will return correct function
+     */
+    @Override
+    boolean isDefinedAt(Object input);
+}
